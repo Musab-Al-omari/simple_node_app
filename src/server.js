8cec39fbe0e30cf4c1f570f3d47e7f5018cc39ae
Musab-Al-omari <musabsomari@gmail.com>
2022-05-04 20:54:03 +0300

    add function

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ const cors = require("cors");
 
 
 
-const {startDatabase} = require("./models/db");
-const authenticationRouter= require("./api/authentication/routes");
-const reflectionRouter= require("./api/reflections/routes");
+const { startDatabase } = require("./models/db");
+const authenticationRouter = require("./api/authentication/routes");
+const reflectionRouter = require("./api/reflections/routes");
 const app = express();
 const PORT = process.env.PORT;
 
@@ -15,11 +15,15 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use("/api/v1",authenticationRouter);
-app.use("/api/v1",reflectionRouter);
+app.use("/api/v1", authenticationRouter);
+app.use("/api/v1", reflectionRouter);
 
 startDatabase().then(() => {
   app.listen(PORT, () => {
     console.log(`running on ${PORT}`);
   });
 });
+
+function name(params) {
+
+}
